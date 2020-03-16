5d4a9e6e09d744f77d30c2f529fae0d61c76e16c
Kelly Jathay <kelly.jathay@gmail.com>
2020-03-16 15:41:47 -0400

    Criando repository e service para Categoria

---
+++ src/main/java/com/kelly/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.kelly.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kelly.cursomc.domain.Categoria;
+import com.kelly.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
